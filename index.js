a25d2e6fa5126ede7b243f9354a19a9337dbee3b
AMANDA RUTHERFOORD <AMANDARUTH08@GMAIL.COM>
2018-05-17 11:05:22 -0500

    stop light exercise

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
-(function() {
-  'use strict';
+let stopButt = document.querySelector('#stopButton');
 
-  // YOUR CODE HERE
-})();
+let count = 0;
+stopButt.addEventListener('click', function() {
+  count++;
+  //count variable increases each click
+  console.log(count);
+  if (count % 2 === 0) {
+    //when count = even
+    document.querySelector('#stopLight').style.backgroundColor = "black";
+  } else {
+    document.querySelector('#stopLight').style.backgroundColor = "red";
+  }
+});
+
+let slowButt = document.querySelector('#slowButton');
+
+slowButt.addEventListener('click', function() {
+  count++;
+  //count variable increases each click
+  console.log(count);
+  if (count % 2 === 0) {
+    //when count = even
+    document.querySelector('#slowLight').style.backgroundColor = "black";
+  } else {
+    document.querySelector('#slowLight').style.backgroundColor = "yellow";
+  }
+});
+
+let goButt = document.querySelector('#goButton');
+
+goButt.addEventListener('click', function() {
+  count++;
+  //count variable increases each click
+  console.log(count);
+  if (count % 2 === 0) {
+    //when count = even
+    document.querySelector('#goLight').style.backgroundColor = "black";
+  } else {
+    document.querySelector('#goLight').style.backgroundColor = "green";
+  }
+});
