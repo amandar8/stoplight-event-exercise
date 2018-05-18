c4098042762b22b5d2c06bb76438cd430e618833
AMANDA RUTHERFOORD <AMANDARUTH08@GMAIL.COM>
2018-05-18 12:12:32 -0500

    stoplight exercise with bonus

--- index.js
+++ index.js
@@ -1,42 +1,92 @@
-let stopButt = document.querySelector('#stopButton');
+(function() {
+  'use strict';
+  document.addEventListener('DOMContentLoaded', function() {
 
-let count = 0;
-stopButt.addEventListener('click', function() {
-  count++;
-  //count variable increases each click
-  console.log(count);
-  if (count % 2 === 0) {
-    //when count = even
-    document.querySelector('#stopLight').style.backgroundColor = "black";
-  } else {
-    document.querySelector('#stopLight').style.backgroundColor = "red";
-  }
-});
+    const stopLight = document.getElementById("stopLight");
+    const slowLight = document.getElementById("slowLight");
+    const goLight = document.getElementById("goLight");
+    const stopButton = document.getElementById("stopButton");
+    const slowButton = document.getElementById("slowButton");
+    const goButton = document.getElementById("goButton");
 
-let slowButt = document.querySelector('#slowButton');
 
-slowButt.addEventListener('click', function() {
-  count++;
-  //count variable increases each click
-  console.log(count);
-  if (count % 2 === 0) {
-    //when count = even
-    document.querySelector('#slowLight').style.backgroundColor = "black";
-  } else {
-    document.querySelector('#slowLight').style.backgroundColor = "yellow";
-  }
-});
+    //Toggle click event listeners
+    stopButton.addEventListener("click", function(event) {
+      event.preventDefault();
+      if (stopLight.classList.contains("bulb")) {
+        stopLight.classList.toggle("stop");
+      }
+    });
 
-let goButt = document.querySelector('#goButton');
+    slowButton.addEventListener("click", function(event) {
+      event.preventDefault();
+      if (slowLight.classList.contains("bulb")) {
+        slowLight.classList.toggle("slow");
+      }
+    });
 
-goButt.addEventListener('click', function() {
-  count++;
-  //count variable increases each click
-  console.log(count);
-  if (count % 2 === 0) {
-    //when count = even
-    document.querySelector('#goLight').style.backgroundColor = "black";
-  } else {
-    document.querySelector('#goLight').style.backgroundColor = "green";
-  }
-});
+    goButton.addEventListener("click", function(event) {
+      event.preventDefault();
+      if (goLight.classList.contains("bulb")) {
+        goLight.classList.toggle("go");
+      }
+    });
+
+// Add new DOM event listeners and handlers to log the mouse state of each button.
+
+
+    stopButton.addEventListener("mouseenter", function() {
+      console.log(`Mouse entered stopButton`);
+    });
+
+    stopButton.addEventListener("mouseleave", function() {
+      console.log(`Mouse left stopButton`);
+    });
+
+    slowButton.addEventListener("mouseenter", function() {
+      console.log(`Mouse entered slowButton`);
+    });
+
+    slowButton.addEventListener("mouseleave", function() {
+      console.log(`Mouse left slowButton`);
+    });
+
+    goButton.addEventListener("mouseenter", function() {
+      console.log(`Mouse entered goButton`);
+    });
+
+    goButton.addEventListener("mouseleave", function() {
+      console.log(`Mouse left goButton`);
+    });
+
+
+// Add one DOM event listener and one handler to log the state of each bulb
+
+
+    document.getElementById("controls").addEventListener("click", function(event) {
+      let id = (event.target.id);
+      if (id === "stopButton") {
+        if (stopLight.classList.contains("stop")) {
+          console.log(`Stoplight Active`);
+        } else {
+          console.log(`Stoplight Inactive`)
+        }
+      }
+      if (id === "slowButton") {
+        if (slowLight.classList.contains("slow")) {
+          console.log(`Slowlight Active`);
+        } else {
+          console.log(`Slowlight Inactive`)
+        }
+      }
+      if (id === "goButton") {
+        if (goLight.classList.contains("go")) {
+          console.log(`Golight Active`);
+        } else {
+          console.log(`Golight Inactive`)
+        }
+      }
+    });
+
+  });
+})();
